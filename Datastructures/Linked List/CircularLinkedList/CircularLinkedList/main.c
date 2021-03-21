9c99b22d8c4902c7457d0cc8b320097d4f2e08ee
Uzair Tariq <s.uzairtariq@gmail.com>
2021-03-21 19:34:02 +0500

    Circular Linked List Completed

--- Datastructures/Linked List/CircularLinkedList/CircularLinkedList/main.c
+++ Datastructures/Linked List/CircularLinkedList/CircularLinkedList/main.c
@@ -36,6 +36,65 @@ void Display(struct Node *p)
     printf("\n");
 }
 
+void Insert(int position, int x)
+{
+    struct Node *t;
+    int i;
+
+    if(position==0)
+    {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        (*t).data=x;
+
+        if(!Head)
+        {
+            Head=t;
+            (*Head).next=t;
+        }
+        else
+        {
+            struct Node *temp=Head;
+            while((*temp).next!=Head)temp=(*temp).next;
+            (*temp).next=t;
+            (*t).next=Head;
+            Head=t;
+        }
+    }
+    else
+    {
+        struct Node *temp=Head;
+        for(int i=0;i<position-1;i++)temp=(*temp).next;
+        t=(struct Node *)malloc(sizeof(struct Node));
+        (*t).data=x;
+        (*t).next=(*temp).next;
+        (*temp).next=t;
+    }
+
+}
+
+void Delete(int position)
+{
+    if(position<=0)
+        return;
+
+    if(position-1==0)
+    {
+        struct Node *p=Head;
+        while((*p).next!=Head)p=(*p).next;
+        (*p).next=(*Head).next;
+        free(Head);
+        Head=(*p).next;
+    }
+    else
+    {
+        struct Node *p=Head;
+        for(int i=1;i<=position-2;i++)p=(*p).next;
+        struct Node *temp=(*p).next;
+        (*p).next=(*temp).next;
+        free(temp);
+    }
+}
+
 int main()
 {
     printf("Circular Linked List!\n");
@@ -43,6 +102,12 @@ int main()
     int A[]={1,2,3,4,5,6,7};
 
     Create(A,7);
+    Display(Head);
+
+    //Insert(3,22);
+    //Display(Head);
+
+    Delete(7);
     Display(Head);
 
     return 0;
